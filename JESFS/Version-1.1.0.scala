78265ad16c6dd604304c49d3d4e034a40fab01e0
SireInsectus <>
2020-09-18 19:49:44 +0000

    Standardizing notebook names and locations

---
+++ JESFS/Version-1.1.0.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC 
+// MAGIC %md # Course: JESFS
+// MAGIC * Version 1.1.0
+// MAGIC * Built 2020-09-02 23:51:27 UTC
+// MAGIC * Git revision: 4aeffdce531541bf35f7b06a04a6e9539c8d2b77
+// MAGIC 
+// MAGIC Copyright © 2020 Databricks, Inc.
